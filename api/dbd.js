ef6b72412abdd49dd9da05fd84cfa65e5776b089
Victor <43808665+Cr1stalz@users.noreply.github.com>
2026-09-24 19:20:12 +0800

    Update dbd.js

--- api/dbd.js
+++ api/dbd.js
@@ -135,7 +135,7 @@ export default async function handler(req, res) {
       ` | ⏱ ${playtime}` +
       ` | 🔪 ${killerRank}` +
       ` | 🧑 ${survivorRank}` +
-      ` | ☠️ Убийства+жертвы: ${totalKills}` +
+      ` | ☠️ Убийства: ${totalKills}` +
       ` | ⚙️ Генераторов: ${Math.round(generators)}` +
       ` | 🩸 Макс. престиж: ${maxPrestige}`;
 
